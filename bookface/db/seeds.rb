9802c382fd1936abf29c98393413449ea8e9bcac
Chris Zhu <chris.zhu12@gmail.com>
2020-08-05 18:13:51 -0700

    added users show page

--- bookface/db/seeds.rb
+++ bookface/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  user = User.create(name: Faker::TvShows::GameOfThrones.character)
+end
+
+100.times do
+  user = User.all.sample
+  user.posts.create(title: Faker::Marketing.buzzwords, body: Faker::TvShows::GameOfThrones.quote)
+end
+
+500.times do
+  post = Post.all.sample
+  user = User.all.sample
+
+  post.comments.create(user: user, body: Faker::Hacker.say_something_smart)
+end
+
+# Create users with no activity
+user = User.create(name: Faker::TvShows::GameOfThrones.character)
+user = User.create(name: Faker::TvShows::GameOfThrones.character)
